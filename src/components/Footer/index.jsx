6788f4bc03f670ea25ae94c8b3ce6577b836b303
prashant0664 <prashantthakran0664@gmail.com>
2023-10-14 13:55:12 +0530

    cimmit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,5 @@
 import logo from "../../assets/logo.webp"
-import { BsGithub, BsFillTelephoneFill, BsFacebook, BsInstagram, BsLinkedin } from 'react-icons/bs'
-import { FaMapMarkerAlt } from 'react-icons/fa'
-import { FaXTwitter } from 'react-icons/fa6'
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 const Footer = () => {
     return (
@@ -20,7 +18,9 @@ const Footer = () => {
                         </p>
                         <div className='flex flex md:flex-row items-center justify-start'>
                             <a href='https://goo.gl/maps/otEPWNLtnYi928Sz6' target='_blank' rel='noreferrer'>
-                                <FaMapMarkerAlt className='hover:text-[#08D4F8] text-[30px] text-[#BECACA]' />
+                                <svg height="30px" fill="#BECACA" width="30px" className="" viewBox="0 0 384 512">
+                                    <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/>
+                                </svg>
                             </a>
                             <div className=' bscflex flex-col items-start justify-center'>
                                 <p className='text-[#BECACA] mx-4 text-md bsc'>
@@ -46,7 +46,7 @@ const Footer = () => {
                         </p>
                         <div className='flex items-center justify-start'>
                             <div className='bg-[#7f7f7f] px-1 pt-1 rounded-lg bsc'>
-                                <BsFillTelephoneFill className='bsc' />
+                                <svg width="17px" fill="white" height="17px" className="bsc" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"  preserveAspectRatio="xMidYMid meet"><path fill="white" d="M34.06 26.407l-3.496-3.496a4.942 4.942 0 0 0-8.34 2.528c-5.765-1.078-11.372-6.662-11.721-11.653a4.908 4.908 0 0 0 2.586-1.36a4.943 4.943 0 0 0 0-6.99L9.594 1.94a4.943 4.943 0 0 0-6.99 0C-7.882 12.426 23.574 43.882 34.06 33.396a4.944 4.944 0 0 0 0-6.989z"></path></svg>
                             </div>
                             <div>
                                 <p className='text-[#BECACA] mx-4 text-md bsc'>
@@ -59,7 +59,7 @@ const Footer = () => {
                         </div>
                         <div className='flex items-center justify-start bsc'>
                             <div className='bg-[#7f7f7f] px-1 pt-1 rounded-lg'>
-                                <BsFillTelephoneFill className='' />
+                                <svg width="17px" fill="white" height="17px" className="bsc" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"  preserveAspectRatio="xMidYMid meet"><path fill="white" d="M34.06 26.407l-3.496-3.496a4.942 4.942 0 0 0-8.34 2.528c-5.765-1.078-11.372-6.662-11.721-11.653a4.908 4.908 0 0 0 2.586-1.36a4.943 4.943 0 0 0 0-6.99L9.594 1.94a4.943 4.943 0 0 0-6.99 0C-7.882 12.426 23.574 43.882 34.06 33.396a4.944 4.944 0 0 0 0-6.989z"></path></svg>
                             </div>
                             <div>
                                 <p className='text-[#BECACA] mx-4 text-md bsc'>
@@ -72,7 +72,7 @@ const Footer = () => {
                         </div>
                         <div className='flex items-center justify-start bsc'>
                             <div className='bg-[#7f7f7f] px-1 pt-1 rounded-lg'>
-                                <BsFillTelephoneFill className='' />
+                                <svg width="17px" fill="white" height="17px" className="bsc" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"  preserveAspectRatio="xMidYMid meet"><path fill="white" d="M34.06 26.407l-3.496-3.496a4.942 4.942 0 0 0-8.34 2.528c-5.765-1.078-11.372-6.662-11.721-11.653a4.908 4.908 0 0 0 2.586-1.36a4.943 4.943 0 0 0 0-6.99L9.594 1.94a4.943 4.943 0 0 0-6.99 0C-7.882 12.426 23.574 43.882 34.06 33.396a4.944 4.944 0 0 0 0-6.989z"></path></svg>
                             </div>
                             <div>
                                 <p className='text-[#BECACA] mx-4 text-md'>
@@ -119,7 +119,9 @@ const Footer = () => {
                         </p>
                         <div className='flex items-center justify-center gap-[20px] margin-bottom-footer2'>
                             <a href='https://www.facebook.com/ieeedtu/' target='_blank' rel='noreferrer'>
-                                <BsFacebook className='text-[23px] hover:text-[#08D4F8]' />
+                                <svg fill="white" width="29px" height="29px" className="text-[23px] bicon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 2.03998C6.5 2.03998 2 6.52998 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.84998C10.44 7.33998 11.93 5.95998 14.22 5.95998C15.31 5.95998 16.45 6.14998 16.45 6.14998V8.61998H15.19C13.95 8.61998 13.56 9.38998 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5878 18.0622 20.3855 19.6099 18.57C21.1576 16.7546 22.0054 14.4456 22 12.06C22 6.52998 17.5 2.03998 12 2.03998Z"/>
+</svg>
                             </a>
                             <a href='https://www.instagram.com/ieee.dtu/' target='_blank' rel='noreferrer'>
                                 <BsInstagram className='text-[23px] hover:text-[#08D4F8]' />
@@ -128,7 +130,11 @@ const Footer = () => {
                                 <BsLinkedin className='text-[23px] hover:text-[#08D4F8]' />
                             </a>
                             <a href='https://twitter.com/dtu_ieee' target='_blank' rel='noreferrer'>
-                                <FaXTwitter className='text-[23px] hover:text-[#08D4F8]' />
+                                <svg height="23px" width="23px" fill="white" className="text-[23px] hover:text-[#08D4F8]" viewBox="0 0 512 512">
+                                    <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+                                    />
+                                </svg>
+                                {/* <FaXTwitter className='text-[23px] hover:text-[#08D4F8]' /> */}
                             </a>
                         </div>
                     </div>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,17 @@
 import logo from "../../assets/logo.webp"
 import logoIEEE from "../../assets/ieeelogo.png"
 import ScrollToTop from "react-scroll-up";
-import { AiOutlineArrowUp } from 'react-icons/ai'
 const Navbar = () => {
     return (
         <>
             <ScrollToTop showUnder={160}>
                 <div >
-                    <AiOutlineArrowUp className="cls" />
+                    <svg fill="#000000" height="40px" width="40px" version="1.1" id="Layer_1" 
+                        viewBox="0 0 330 330" space="preserve" className="cls">
+                        <path id="XMLID_21_" d="M213.107,41.894l-37.5-37.5c-5.857-5.858-15.355-5.858-21.213,0l-37.5,37.5
+	c-4.29,4.29-5.573,10.742-3.252,16.347c2.322,5.605,7.792,9.26,13.858,9.26H150V315c0,8.284,6.716,15,15,15c8.284,0,15-6.716,15-15
+	V67.5h22.5c6.067,0,11.537-3.655,13.858-9.26C218.68,52.635,217.397,46.184,213.107,41.894z"/>
+                    </svg>
                 </div>
             </ScrollToTop>
             <div className='flex flex-row flex-nowrap w-full items-center justify-between bg-[#1B1B1B] z-10 text-[#fbf7f7] fixed top-0 ssm:my-[0px]'>
